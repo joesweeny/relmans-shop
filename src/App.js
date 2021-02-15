c27e8c1d70667eb4b0ef83f2c1b6f66ae8d4e2c0
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2021-02-15 12:19:06 +0000

    Add linting rules

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const App = () => {
-  return (
-    <div className="App">
-      Relmans Shop
-    </div>
-  );
-}
+  return <div className="App">Relmans Shop</div>;
+};
 
 export default App;
