5f93ca352970f4e8cceab085c001251d172b7fb1
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2021-02-16 19:05:44 +0000

    Add client logic

--- src/gateway/client.js
+++ src/gateway/client.js
@@ -1,31 +1,11 @@
-import { Auth } from 'aws-amplify';
 import axios from './axios-client';
+
+export const getCategories = async () => {
+  const response = await axios.get('/category');
+  return response.data.data.categories;
+};
 
 export const getProducts = async () => {
   const response = await axios.get('/product');
   return response.data.data.products;
-};
-
-export const updateProduct = async (id, payload) => {
-  const token = (await Auth.currentSession()).getIdToken().getJwtToken();
-
-  await axios.patch(`/product/${id}`, payload, {
-    headers: {
-      Authorization: `${token}`,
-    },
-  });
-};
-
-export const updatePrice = async (id, price) => {
-  const token = (await Auth.currentSession()).getIdToken().getJwtToken();
-
-  await axios.patch(
-    `/price/${id}`,
-    { value: price },
-    {
-      headers: {
-        Authorization: `${token}`,
-      },
-    }
-  );
 };
