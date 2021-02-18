8b6e775a11c93861f6de15ac92ea063f0128e9ee
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2021-02-18 13:53:28 +0000

    Add scroll to top functionality

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import React, { useLayoutEffect } from 'react';
+import { Redirect, Route, Switch, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 
 import Category from '../Category/Category';
@@ -9,14 +9,22 @@ const RoutesWrapper = styled.div`
   flex: 1;
 `;
 
-const Routes = () => (
-  <RoutesWrapper>
-    <Switch>
-      <Route path="/category/:id" component={Category} />
-      <Route path="/" component={Home} />
-      <Redirect to="/" />
-    </Switch>
-  </RoutesWrapper>
-);
+const Routes = () => {
+  const location = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
+  return (
+    <RoutesWrapper>
+      <Switch>
+        <Route path="/category/:id" component={Category} />
+        <Route path="/" component={Home} />
+        <Redirect to="/" />
+      </Switch>
+    </RoutesWrapper>
+  );
+};
 
 export default Routes;
