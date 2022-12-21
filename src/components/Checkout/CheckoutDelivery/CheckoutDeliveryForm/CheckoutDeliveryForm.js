47bff385ae5e257880252ff8d2f17bc640674d50
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2022-12-21 13:53:27 +0000

    Fix linting issues

--- src/components/Checkout/CheckoutDelivery/CheckoutDeliveryForm/CheckoutDeliveryForm.js
+++ src/components/Checkout/CheckoutDelivery/CheckoutDeliveryForm/CheckoutDeliveryForm.js
@@ -49,9 +49,9 @@ const CheckoutDeliveryForm = (props) => {
       {isValid && isSelected ? (
         <Info>
           <p>
-            All Christmas delivery slots on 22nd and 23rd December are full. We are also
-            closed on 27th December 2022 and 3rd January 2023. Thank you for
-            continued business.
+            All Christmas delivery slots on 22nd and 23rd December are full. We
+            are also closed on 27th December 2022 and 3rd January 2023. Thank
+            you for continued business.
           </p>
           <span>Please select a date for delivery.</span>
           <span>
